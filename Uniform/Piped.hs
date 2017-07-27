5b25d88ba35f4a6f3e802f4166edbb09a6566525
Andrew Frank <frank@geoinfo.tuwien.ac.at>
2017-07-28 00:25:55 +0200

    added debug info when seemingly not all files are found

--- Uniform/Piped.hs
+++ Uniform/Piped.hs
@@ -63,7 +63,10 @@ getRecursiveContents  fp = do
             if symLink
                 then  Pipe.lift $ putIOwords ["recurseDir symlink", showT fp]
                 else do
-                    (dirs, files) <- Pipe.lift   $ listDir  fp
+                    (dirs, files) <- Pipe.lift $ listDir  fp
+                    Pipe.lift $ putIOwords ["recurseDir files\n", showT files]
+                    Pipe.lift $ putIOwords ["recurseDir directories\n", showT dirs]
+
                     Prelude.mapM_ (Pipe.yield) (sort files)
                     Prelude.mapM_ (getRecursiveContents) (sort dirs)
                     return ()--    where processOneFile fp = Pipe.yield fp
